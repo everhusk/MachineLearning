39bd02d9bd57af6f9e93c1654a63242fd91378b7
Sanghera <el.sanghera@gmail.com>
2015-04-13 23:16:43 -0400

    Quiz #3a

---
+++ decision_tree/dt_author_id.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+""" 
+    this is the code to accompany the Lesson 3 (decision tree) mini-project
+
+    use an DT to identify emails from the Enron corpus by their authors
+    
+    Sara has label 0
+    Chris has label 1
+
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+
+
+#########################################################
+### your code goes here ###
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+prediction = clf.predict(features_test)
+print accuracy_score(prediction, labels_test)
+## Accuracy: 0.977815699659
+#########################################################
+
+
