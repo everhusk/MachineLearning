878b86f467cb984402d8639a1182e863549d100d
Sanghera <el.sanghera@gmail.com>
2015-04-22 18:33:52 -0400

    Quiz #10 - Text Learning

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -21,6 +21,9 @@ from parse_out_email_text import parseOutText
     the data is stored in lists and packed away in pickle files at the end
 
 """
+### Parse out test email
+testEmail = open("test_email.txt", "r")
+print parseOutText(testEmail)
 
 
 from_sara  = open("from_sara.txt", "r")
@@ -47,16 +50,21 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-
+            words = parseOutText(email)
+            remove  = ["sara", "shackleton", "chris", "germani"]
+            #list_rep  = ["sara", "shackleton", "chris", "germani","sshacklensf","cgermannsf"]
+            for word in remove:
+                words = words.replace(word,"")
             ### append the text to word_data
-
+            word_data.append(words)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            from_data.append(0 if name == "sara" else 1)
 
             email.close()
+
+print "Entry #152: ", word_data[152]
 
 print "emails processed"
 from_sara.close()
@@ -66,9 +74,17 @@ pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
-
-
-
 ### in Part 4, do TfIdf vectorization here
+from nltk.corpus import stopwords
+sw = stopwords.words("english")
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words="english",lowercase=True)
+vectorizer.fit_transform(word_data)
+# bag_words = vectorizer.transform(word_data)
 
+# Get how many unique words there are in the emails
+print len(vectorizer.get_feature_names())
+
+# Word number 34597
+print vectorizer.get_feature_names()[34597]
