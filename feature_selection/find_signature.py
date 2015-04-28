94b32f707c6c39ce6bbf3465096518e4de07da90
Sanghera <el.sanghera@gmail.com>
2015-04-28 14:31:19 -0400

    Quiz #11 - Feature Selection

---
+++ feature_selection/find_signature.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import pickle
+import numpy
+numpy.random.seed(42)
+
+
+### the words (features) and authors (labels), already largely processed
+### these files should have been created from the previous (Lesson 10) mini-project.
+words_file = "../text_learning/your_word_data.pkl" 
+authors_file = "../text_learning/your_email_authors.pkl"
+word_data = pickle.load( open(words_file, "r"))
+authors = pickle.load( open(authors_file, "r") )
+
+
+### test_size is the percentage of events assigned to the test set (remainder go into training)
+from sklearn import cross_validation
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
+                             stop_words='english')
+features_train = vectorizer.fit_transform(features_train).toarray()
+features_test  = vectorizer.transform(features_test).toarray()
+
+
+### a classic way to overfit is to use a small number
+### of data points and a large number of features
+### train on only 150 events to put ourselves in this regime
+features_train = features_train[:150]
+labels_train   = labels_train[:150]
+
+
+### your code goes here
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+print "Score: ",clf.score(features_test,labels_test)
+### Accuracy 1.0 on overfit data
+importances = clf.feature_importances_
+import numpy as np
+indices = np.argsort(importances)[::-1]
+print 'Feature Ranking: '
+for i in range(10):
+    print "{} feature no.{} ({})".format(i+1,indices[i],importances[indices[i]])
+
